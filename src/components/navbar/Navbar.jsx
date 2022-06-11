91cf223f69edd189537afbfa7f47c349825c31c6
Damilola Bada <badadamilola@gmx.de>
2022-06-11 17:44:34 +0200

    navbar & header implemented, css-tricks: backdrop-filter

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
+import Logo from "../../assets/Logo.svg";
 import "./navbar.css";
 
+// Making the navigation links more compact and reuseable
+const Menu = () => {
+  return (
+    <>
+      <p>
+        {" "}
+        <a href="#home">Home</a>{" "}
+      </p>
+      <p>
+        {" "}
+        <a href="#">Features</a>{" "}
+      </p>
+      <p>
+        {" "}
+        <a href="#">Service</a>{" "}
+      </p>
+      <p>
+        {" "}
+        <a href="#">Listed</a>{" "}
+      </p>
+      <p>
+        {" "}
+        <a href="#">Contact</a>{" "}
+      </p>
+    </>
+  );
+};
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // To set the state of the menu icon so it can be toggled
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    // Desktop Menu Style
+    <div className="besnik__navbar">
+      <div className="besnik__navbar-links">
+        <div className="besnik__navabr-links_logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="besnik__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="besnik__navbar-sign">
+        <p>Sign up</p>
+        <button type="button">Register</button>
+      </div>
+      {/* Mobile Menu Style */}
+      <div className="besnik__navbar-mobile">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#0f1b4c"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#0f1b4c"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="besnik__navbar-mobile_container swing-in-right-bck">
+            <div className="besnik__navbar-mobile_container-links">
+              <Menu />
+              <div className="besnik__navbar-mobile_container-links-sign">
+                <p>Sign up</p>
+                <button type="button">Register</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import { Navbar } from "../../components";
+import House from "../../assets/house illustration.svg";
 import "./header.css";
 
 const Header = () => {
   return (
     <div>
       <Navbar />
-      Header
+      <div className="besnik__header section__padding" id="home">
+        <div className="besnik__header-content">
+          <p className="besnik__header-content_subtext">
+            Welcome to Besnik Agency
+          </p>
+          <h1 className="besnik__header-content_maintext">
+            Discover a place you'll love to live.
+          </h1>
+          <p className="besnik__header-content_desctext">
+            Get the best real estate deals first, before they hit the mass
+            market! <span>hot foreclosure deals</span> with one simple search
+          </p>
+          <div className="besnik__header-content__btn-container">
+            <button type="button">More About Us</button>
+          </div>
+        </div>
+        <div className="besnik__header-image">
+          <img src={House} alt="ai" />
+        </div>
+      </div>
     </div>
   );
 };
