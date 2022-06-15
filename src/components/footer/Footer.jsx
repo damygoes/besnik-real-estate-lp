9ae845acbfbf727d12cb05808136e001ad669a02
Damilola Bada <badadamilola@gmx.de>
2022-06-15 15:24:53 +0200

    Done: all components, styling and media queries. Left: animations.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,28 +10,28 @@ const Footer = () => {
   return (
     <div className="besnik__footer section__padding">
       <div className="besnik__footer-links">
-        <div className="besnik__footer-links_div">
+        <div className="besnik__footer-links_div flipped">
           <h4>Product</h4>
           <p>Listing</p>
           <p>Property</p>
           <p>Agents</p>
           <p>Blog</p>
         </div>
-        <div className="besnik__footer-links_div">
+        <div className="besnik__footer-links_div unflipped">
           <h4>Resources</h4>
           <p>Our Homes </p>
           <p>Member Stories</p>
           <p>Video</p>
           <p>Free Trial</p>
         </div>
-        <div className="besnik__footer-links_div">
+        <div className="besnik__footer-links_div flipped">
           <h4>Company</h4>
           <p>Partnerships</p>
           <p>Terms of use</p>
           <p>Privacy</p>
           <p>Sitemap</p>
         </div>
-        <div className="besnik__footer-links_div">
+        <div className="besnik__footer-links_div unflipped">
           <h4>Get in touch</h4>
           <p>You’ll find your next home, in any style you prefer.</p>
           <div>
